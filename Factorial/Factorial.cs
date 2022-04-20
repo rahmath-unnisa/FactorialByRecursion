90af115d5b7630524db0dbba4ae431d3bf2a5271
Rahmath-Unnisa <asfiarehmath@gmail.com>
2022-04-20 20:36:46 +0530

    [Rahmath]Add: Factorial By Recursion

---
+++ Factorial/Factorial.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorial
+{
+    public class Factorial
+    {
+            int result = 1;
+            int num = Convert.ToInt32(Console.ReadLine());
+            public int Recursion()
+            {
+                if (num == 0)
+                {
+                    return 1;
+                }
+                result = result * num;
+                num--;
+                Recursion();
+                return result;
+            }
+
+
+        }
+    }
---
+++ Factorial/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Factorial
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Factorial factorial = new Factorial();
+            int answer = factorial.Recursion();
+            Console.WriteLine("Factorial of given number " + answer);
+            Console.ReadLine();
+        }
+    }
+}
